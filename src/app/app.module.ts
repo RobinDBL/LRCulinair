372d63ca4a122e21198352814e620e4aa0fcd2f8
Robin Deblauwe <robindeblauwe@telenet.be>
2022-02-20 22:13:31 +0100

    general updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { WieComponent } from './wie/wie.component';
+import { FotosComponent } from './fotos/fotos.component';
+import { ContactComponent } from './contact/contact.component';
+import { CookiesComponent } from './cookies/cookies.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    WieComponent,
+    FotosComponent,
+    ContactComponent,
+    CookiesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
