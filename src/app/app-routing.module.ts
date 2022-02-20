8ee0fdca0e81c2c67222f604ba2840ced1e71fd0
Robin Deblauwe <robindeblauwe@telenet.be>
2022-02-20 22:11:53 +0100

    updated routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
+import { ContactComponent } from './contact/contact.component';
+import { FotosComponent } from './fotos/fotos.component';
+import { WieComponent } from './wie/wie.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'fotos', component: FotosComponent},
+  { path: 'wie', component: WieComponent},
+  { path: 'contact', component: ContactComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
