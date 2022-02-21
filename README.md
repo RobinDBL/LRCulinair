01210ebd1178b3ccbd5d7c68336dcacd373a93b3
Robin Deblauwe <robindeblauwe@telenet.be>
2022-02-21 11:18:22 +0100

    added CI/CD pipelines

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# LrCulinair
+# lr-culinair
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.2.3.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'lrCulinair'`, () => {
+  it(`should have as title 'lr-culinair'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('lrCulinair');
+    expect(app.title).toEqual('lr-culinair');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('lrCulinair app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('lr-culinair app is running!');
   });
 });
